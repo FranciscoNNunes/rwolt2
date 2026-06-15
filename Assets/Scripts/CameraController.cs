2ccb2c6f615e586e7b6ac9ca517bdde209d69a9d
FranciscoNNunes <francisco60574256@alunos.sc.senac.br>
2026-06-15 14:40:12 -0300

    Alterações

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
-public class cameraFollow : MonoBehaviour
+public class CameraFollow1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed;
+    private float currentPostX;
+    private Vector3 velocity = Vector3.zero;
 
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position = new Vector3(player.position.x + lookAhead, player.position.y, player.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPostX =  _newRoom.position.x;
     }
 }
