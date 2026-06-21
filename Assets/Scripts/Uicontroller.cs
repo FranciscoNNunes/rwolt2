694b0e69d854ba3d2e6a0b7e5506f74ffde9580f
FranciscoNNunes <francisco60574256@alunos.sc.senac.br>
2026-06-21 18:42:50 -0300

    Sistema de pause adicionado

---
+++ Assets/Scripts/Uicontroller.cs
@@ -0,0 +1,58 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class Uicontroller : MonoBehaviour
+{
+
+
+    public GameObject pauseMenu;
+
+    private bool paused = false;
+
+    void Start()
+    {
+        HidePauseMenu();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (paused)
+                HidePauseMenu();
+            else
+                ShowPauseMenu();
+        }
+    }
+    private void ShowPauseMenu()
+    {
+        paused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    private void HidePauseMenu()
+    {
+        paused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void BTN_Resume()
+    {
+        HidePauseMenu();
+    }
+    public void BTN_Quit()
+    {
+     Application.Quit();
+        #if UNITY_EDITOR
+
+            EditorApplication.ExitPlaymode();
+#endif
+    
+    }
+    public void MainMenu() 
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
