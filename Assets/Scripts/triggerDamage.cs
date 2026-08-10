e9231607d3ae748f04e7a711637e0af6a82ad024
FranciscoNNunes <francisco60574256@alunos.sc.senac.br>
2026-08-10 16:35:48 -0300

    Mudanças na animação da SampleScene

--- Assets/Scripts/triggerDamage.cs
+++ Assets/Scripts/triggerDamage.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Triger : MonoBehaviour
 {
     public heartSystem heart;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Ontriggerenter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player") 
         {
